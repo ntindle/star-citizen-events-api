5d8074eb520e1a26f8f09c0f8254a248c49a3efb
Nicholas Tindle <nicholas.tindle@dialexa.com>
2023-03-03 16:52:02 -0600

    fix: disable swagger

--- Program.cs
+++ Program.cs
@@ -36,16 +36,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "SCEAPI v1");
-        options.RoutePrefix = "";
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "SCEAPI v1");
+    options.RoutePrefix = "";
+});
+// }
 
 app.UseHttpsRedirection();
 
